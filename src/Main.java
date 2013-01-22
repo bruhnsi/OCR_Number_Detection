894e757874fc871fa922236a8afa4b70f47e7f07
Niki <Niki@Niki-PC>
2013-01-22 20:28:20 +0100

    added Shuffle Array Method to Main

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,17 @@ public class Main {
 		// TODO Auto-generated method stub
 		DataProvider dataProvider = new DataProvider("./bin/test.h5");
 		ImageData[] data = dataProvider.readData();
+		shuffle(data);
 		System.out.print(data[0]);
 	}
-
+	
+	// Shuffles an array
+    private static <T> void shuffle(T[] array) {
+        for (int i = array.length; i > 1; i--) {
+                T temp = array[i - 1];
+                int randIx = (int) (Math.random() * i);
+                array[i - 1] = array[randIx];
+                array[randIx] = temp;
+        }
+}
 }
