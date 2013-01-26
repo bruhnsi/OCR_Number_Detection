52403b871fad077929a07a89d601a49cbaa90c25
Stefan Bruhns <bruhnsi@gmx.net>
2013-01-26 21:05:55 +0000

    db4o save load

---
+++ src/Data/Db4oNetworkPersister.java
@@ -0,0 +1,31 @@
+package Data;
+
+import NeuralNetwork.Network;
+
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+import com.db4o.config.EmbeddedConfiguration;
+
+public class Db4oNetworkPersister {
+	
+	private ObjectContainer db;
+	
+	public Db4oNetworkPersister(String path)
+	{
+		db = Db4oEmbedded.openFile(path);
+	}
+	
+	public void storeNetwork(Network net)
+	{
+		db.store(net);
+	}
+	
+	public Network[] loadNetworks()
+	{
+		ObjectSet result = db.queryByExample(Network.class);
+		return (Network[]) result.toArray();
+	}
+	
+	
+}
